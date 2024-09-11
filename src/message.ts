102d39ada17e1482232e97e8c2e426e8d6672392
GamerYuan <f2pplayer0801@gmail.com>
2024-09-11 13:27:52 +0800

    opt: improve wall time

--- src/message.ts
+++ src/message.ts
@@ -1,150 +1,156 @@
-import { Message, Embed } from './types/discord_embed'
-import { PostData } from './types/hoyolab_post'
+import { Message, Embed } from './types/discord_embed';
+import { PostData } from './types/hoyolab_post';
 import { LANG_DETAILS, LANG_ABBR, FOOTER_TEXT, DEFAULT_HEADER_DICT } from './types/constants';
 
 const POST_LENGTH: number = 500;
 const POST_DATA = 'https://bbs-api-os.hoyolab.com/community/post/wapi/getPostFull';
-const URL_RE = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?([^ ])+");
+const URL_RE = new RegExp('([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?([^ ])+');
 
 async function buildMessage(postID: number, lang: string): Promise<Embed> {
-    const postDetail = await fetchPostDetail(postID, lang);
-    const base: Partial<Embed> = {
-        color: 7436279,
-        url: `https://www.hoyolab.com/article/${postDetail.data.post.post.post_id}`,
-        author: {
-            name: postDetail.data.post.user.nickname,
-            url: `https://www.hoyolab.com/accountCenter/postList?id=${postDetail.data.post.user.uid}`,
-            icon_url: postDetail.data.post.user.avatar_url,
-        },
-        footer: {
-            text: postDetail.data.post.post.origin_lang != postDetail.data.post.post.lang ? `HoYoLAB · ${FOOTER_TEXT[LANG_ABBR.indexOf(postDetail.data.post.post.lang)]}` : 'HoYoLAB',
-            icon_url: 'https://media.discordapp.net/attachments/943106145546014732/1137378106135564358/favicon.png',
-        },
-        timestamp: new Date(postDetail.data.post.post.created_at * 1000),
-        title: postDetail.data.post.post.subject,
-        description: await buildPostDetail(postDetail),
-    }
+	const postDetail = await fetchPostDetail(postID, lang);
+	const base: Partial<Embed> = {
+		color: 7436279,
+		url: `https://www.hoyolab.com/article/${postDetail.data.post.post.post_id}`,
+		author: {
+			name: postDetail.data.post.user.nickname,
+			url: `https://www.hoyolab.com/accountCenter/postList?id=${postDetail.data.post.user.uid}`,
+			icon_url: postDetail.data.post.user.avatar_url,
+		},
+		footer: {
+			text:
+				postDetail.data.post.post.origin_lang != postDetail.data.post.post.lang
+					? `HoYoLAB · ${FOOTER_TEXT[LANG_ABBR.indexOf(postDetail.data.post.post.lang)]}`
+					: 'HoYoLAB',
+			icon_url: 'https://media.discordapp.net/attachments/943106145546014732/1137378106135564358/favicon.png',
+		},
+		timestamp: new Date(postDetail.data.post.post.created_at * 1000),
+		title: postDetail.data.post.post.subject,
+		description: await buildPostDetail(postDetail),
+	};
 
-    if (postDetail.data.post.cover_list.length > 0) {
-        base.image = {
-            url: postDetail.data.post.cover_list[0].url,
-        }
-    } else if (postDetail.data.post.image_list.length > 0) {
-        base.image = {
-            url: postDetail.data.post.image_list[0].url,
-        }
-    }
+	if (postDetail.data.post.cover_list.length > 0) {
+		base.image = {
+			url: postDetail.data.post.cover_list[0].url,
+		};
+	} else if (postDetail.data.post.image_list.length > 0) {
+		base.image = {
+			url: postDetail.data.post.image_list[0].url,
+		};
+	}
 
-    return base;
+	return base;
 }
 
 export async function pushToDiscord(posts: number[], webhooks: string, roles: readonly string[] = [], lang: string = 'en-us') {
-    const list = webhooks.split(',');
-    // Sends from oldest to newest
-    const embeds = await Promise.all(posts.slice(0, 10).map(async post => await buildMessage(post, lang)).reverse());
-    const webhookPayload: Message = {
-        embeds,
-        allowed_mentions: {
-            parse: []
-        }
-    }
-    if (roles && roles.length > 0) {
-        webhookPayload.content = roles.map(role => `@<&${role}>`).join('');
-        webhookPayload.allowed_mentions = {
-            parse: ['roles'],
-        }
-    }
-    for (let i = 0; i < list.length; i++) {
-        await pushMessage(webhookPayload, list[i]);
-    }
+	const list = webhooks.split(',');
+	// Sends from oldest to newest
+	const embeds = await Promise.all(
+		posts
+			.slice(0, 10)
+			.map(async (post) => await buildMessage(post, lang))
+			.reverse()
+	);
+	const webhookPayload: Message = {
+		embeds,
+		allowed_mentions: {
+			parse: [],
+		},
+	};
+	if (roles && roles.length > 0) {
+		webhookPayload.content = roles.map((role) => `@<&${role}>`).join('');
+		webhookPayload.allowed_mentions = {
+			parse: ['roles'],
+		};
+	}
+
+	const payloads = [];
+	for (let i = 0; i < list.length; i++) {
+		payloads.push(pushMessage(webhookPayload, list[i]));
+	}
+	await Promise.all(payloads);
 }
 
-export async function pushMessage(content: any, webhook: string) {
-    console.log(`Payload: ${JSON.stringify(content)}`);
-    const response = await fetch(webhook, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-        },
-        body: JSON.stringify(content),
-    })
-    if (!response.ok) {
-        console.error(`Webhook Failed`, response.status, await response.json())
-    }
+export async function pushMessage(content: Message, webhook: string) {
+	const bodyStr = JSON.stringify(content);
+	console.log(`Payload: ${bodyStr}`);
+	const response = await fetch(webhook, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: '*/*',
+		},
+		body: bodyStr,
+	});
+	if (!response.ok) {
+		const text = await response.text();
+		console.error(`Webhook Failed`, response.status, text);
+		throw new Error(`Webhook Failed: ${response.status}, ${text}`);
+	}
 }
 
 export async function fetchPostDetail(postID: number, lang: string): Promise<PostData> {
-    const target = `${POST_DATA}?post_id=${postID}`;
-    const requestHeader = DEFAULT_HEADER_DICT;
-    requestHeader['X-Rpc-Language'] = lang;
-    const response = await fetch(target, {
-        headers: requestHeader,
-        method: 'GET',
-    });
+	const target = `${POST_DATA}?post_id=${postID}`;
+	const requestHeader = DEFAULT_HEADER_DICT;
+	requestHeader['X-Rpc-Language'] = lang;
+	const response = await fetch(target, {
+		headers: requestHeader,
+		method: 'GET',
+	});
 
-    return await response.json<PostData>();
+	return await response.json<PostData>();
 }
 
 export async function buildPostDetail(post: PostData): Promise<string> {
-    if (!post.data.post.post.structured_content) {
-        return "";
-    }
-    const content = JSON.parse(post.data.post.post.structured_content);
-    if (Array.isArray(content)) {
-        const ret = content.map(processElement);
-        const retLen = ret.length;
-        let final: string = "";
-        let currLen: number = 0;
-        for (let i = 0; i < retLen; i++) {
-            let insertText = ret[i]
-            if (currLen + insertText.length > POST_LENGTH) {
-                const detailString = LANG_DETAILS[LANG_ABBR.findIndex(x => x === post.data.post.post.lang)];
-                if (insertText.match(URL_RE)) {
-                    final += `${insertText.trim()}\n\n${detailString}`;
-                    break;
-                }
-                final += insertText;
-                final = `${final.substring(0, POST_LENGTH)}...\n\n${detailString}`;
-                break;
-            }
-            // Removes Discord markdown
-            if (!insertText.match(URL_RE)) {
-                insertText = insertText.replace(/[.*-]/g, (match) => `\\${match}`);
-            }
-            final += insertText;
-            currLen += insertText.length;
-        }
-        return final.trimStart();
-
-    }
-    return "";
-}
-
-export async function sendErrorMessage(message: string, webhook: string) {
-    const payload: Message = {
-        content: message,
-    }
-    await pushMessage(payload, webhook);
+	if (!post.data.post.post.structured_content) {
+		return '';
+	}
+	const content = JSON.parse(post.data.post.post.structured_content);
+	if (Array.isArray(content)) {
+		const ret = content.map(processElement);
+		const retLen = ret.length;
+		let final: string = '';
+		let currLen: number = 0;
+		for (let i = 0; i < retLen; i++) {
+			let insertText = ret[i];
+			if (currLen + insertText.length > POST_LENGTH) {
+				const detailString = LANG_DETAILS[LANG_ABBR.findIndex((x) => x === post.data.post.post.lang)];
+				if (insertText.match(URL_RE)) {
+					final += `${insertText.trim()}\n\n${detailString}`;
+					break;
+				}
+				final += insertText;
+				final = `${final.substring(0, POST_LENGTH)}...\n\n${detailString}`;
+				break;
+			}
+			// Removes Discord markdown
+			if (!insertText.match(URL_RE)) {
+				insertText = insertText.replace(/[.*-]/g, (match) => `\\${match}`);
+			}
+			final += insertText;
+			currLen += insertText.length;
+		}
+		return final.trimStart();
+	}
+	return '';
 }
 
 function processElement(element: any): string {
-    const insertVal = element.hasOwnProperty('insert') ? element['insert'] : '';
+	const insertVal = element.hasOwnProperty('insert') ? element['insert'] : '';
 
-    if (typeof insertVal !== 'string') {
-        return '';
-    }
+	if (typeof insertVal !== 'string') {
+		return '';
+	}
 
-    if (element.hasOwnProperty('attributes') && element['attributes'].hasOwnProperty('link')) {
-        const link = element['attributes']['link'];
-        if (link.trim() === insertVal.trim()) {
-            return insertVal;
-        } else if (insertVal.trim().match(URL_RE)) {
-            return link.trim();
-        } else {
-            return `[${insertVal}](${link.trim()})`;
-        }
-    } else {
-        return insertVal;
-    }
+	if (element.hasOwnProperty('attributes') && element['attributes'].hasOwnProperty('link')) {
+		const link = element['attributes']['link'];
+		if (link.trim() === insertVal.trim()) {
+			return insertVal;
+		} else if (insertVal.trim().match(URL_RE)) {
+			return link.trim();
+		} else {
+			return `[${insertVal}](${link.trim()})`;
+		}
+	} else {
+		return insertVal;
+	}
 }
--- src/triggers.ts
+++ src/triggers.ts
@@ -1,116 +1,102 @@
 import { Env } from './worker';
 import { HoyolabList, PostSummary } from './types/hoyolab_api';
 import { SETTINGS } from './user-config';
-import { pushToDiscord, sendErrorMessage } from './message';
+import { pushToDiscord } from './message';
 import { LANG_ABBR, DEFAULT_HEADER_DICT } from './types/constants';
 
 let env: Env;
-const API = 'https://bbs-api-os.hoyolab.com/community/post/wapi/userPost'
+const API = 'https://bbs-api-os.hoyolab.com/community/post/wapi/userPost';
 
 export async function fetchMessageList(userID: string, lang: string = 'en-us'): Promise<PostSummary[]> {
-    const target = `${API}?size=10&uid=${userID}`;
+	const target = `${API}?size=10&uid=${userID}`;
 
-    const requestHeader = DEFAULT_HEADER_DICT;
-    requestHeader['X-Rpc-Language'] = lang;
-    const response = await fetch(target, {
-        headers: requestHeader,
-        method: 'GET',
-    });
+	const requestHeader = DEFAULT_HEADER_DICT;
+	requestHeader['X-Rpc-Language'] = lang;
+	const response = await fetch(target, {
+		headers: requestHeader,
+		method: 'GET',
+	});
 
-    const json = await response.json<HoyolabList>();
-    let list = json.data.list;
+	const json = await response.json<HoyolabList>();
+	let list = json.data.list;
 
-    if (list.length > 0 && (list[0].tags.is_user_top && Number(list[0].post.post_id) < Number(list[1].post.post_id))) {
-        list = list.slice(1);
-    }
+	if (list.length > 0 && list[0].tags.is_user_top && Number(list[0].post.post_id) < Number(list[1].post.post_id)) {
+		list = list.slice(1);
+	}
 
-    return list;
+	return list;
 }
 
 // Get new posts posted since last trigger
 export function filterPost(list: number[], last: number): number[] {
-    const filtered = list.filter(x => x > last);
-    return filtered;
+	const filtered = list.filter((x) => x > last);
+	return filtered;
 }
 
-async function processSingleUser(userID: string, webhooks: readonly string[], roles:{ readonly [k in typeof webhooks[number]]: readonly string[]}, language:{ readonly [k in typeof webhooks[number]]: string}, force = false) {
-    console.log(`Processing user: ${userID}`);
-    const KV_KEY = `feed_${userID}`;
-    const updateKV = async (list: number[]) => {
-        await env.POST_CACHE.put(KV_KEY, list[0].toString());
-    }
+async function processSingleUser(
+	userID: string,
+	webhooks: readonly string[],
+	roles: { readonly [k in (typeof webhooks)[number]]: readonly string[] },
+	language: { readonly [k in (typeof webhooks)[number]]: string },
+	force = false
+) {
+	console.log(`Processing user: ${userID}`);
+	const KV_KEY = `feed_${userID}`;
+	const updateKV = async (list: number[]) => {
+		await env.POST_CACHE.put(KV_KEY, list[0].toString());
+	};
 
-    let list = (await fetchMessageList(userID)).flatMap(x => Number(x.post.post_id)); 
-    if (list.length == 0) {
-        return;
-    }
-    const last = await env.POST_CACHE.get<number>(KV_KEY);
+	let fetchMessage = fetchMessageList(userID).then((m) => m.flatMap((x) => Number(x.post.post_id)));
+	const postCache = env.POST_CACHE.get<number>(KV_KEY);
 
-    // First fetch from user
-    if (!last) {
-        await updateKV(list);
-        return;
-    }
+	const [list, last] = await Promise.all([fetchMessage, postCache]);
 
-    let newPosts = filterPost(list, last);
-    if (newPosts.length < 1) {
-        if (!force) {
-            return;
-        }
-    }
-    console.log(`Found ${newPosts.length} new posts.`);
+	if (list.length == 0) {
+		return;
+	}
 
-    for (let key of webhooks) {
-        const webhook = await env.WEBHOOKS.get(key)
-        if (!webhook) {
-            console.error(`No Webhook URL for key: ${key}, UID: ${userID} skipped`);
-            continue;
-        }
-        console.log(language[key]);
-        await pushToDiscord(newPosts, webhook, roles[key] ?? [], language[key] == '' || !LANG_ABBR.includes(language[key].trim()) ? 'en-us' : language[key]);
-    }
-    console.log(`Sent ${newPosts.length} posts.`)
-    await updateKV(list);
-}
+	// First fetch from user
+	if (!last) {
+		await updateKV(list);
+		return;
+	}
 
-async function processError(message: string, webhooks: readonly string[]) {
-    const KEY = "last_error_time"
-    const now = Date.now();
-    const last = await env.POST_CACHE.get(KEY);
-    if (!last) {
-        await env.POST_CACHE.put(KEY, now.toString())
-        for (let key of webhooks) {
-            const webhook = await env.WEBHOOKS.get(key)
-            if (!webhook) {
-                console.log(`No Webhook URL for key: ${key}, send error message skipped`);
-                continue;
-            }
-            await sendErrorMessage(message, webhook)
-        }
-        return
-      }
-    
-      if (now - parseInt(last) > 3600000) {
-        for (let key of webhooks) {
-            const webhook = await env.WEBHOOKS.get(key)
-            if (!webhook) {
-                console.log(`No Webhook URL for key: ${key}, send error message skipped`);
-                continue;
-            }
-            await sendErrorMessage(message, webhook)
-        }
-        await env.POST_CACHE.put(KEY, Date.now().toString())
-      }
+	let newPosts = filterPost(list, last);
+	if (newPosts.length < 1) {
+		if (!force) {
+			return;
+		}
+	}
+	console.log(`Found ${newPosts.length} new posts.`);
+
+	const pushJobs = [];
+
+	for (let key of webhooks) {
+		const webhook = await env.WEBHOOKS.get(key);
+		if (!webhook) {
+			console.error(`No Webhook URL for key: ${key}, UID: ${userID} skipped`);
+			continue;
+		}
+		pushJobs.push(
+			pushToDiscord(
+				newPosts,
+				webhook,
+				roles[key] ?? [],
+				language[key] == '' || !LANG_ABBR.includes(language[key].trim()) ? 'en-us' : language[key]
+			)
+		);
+	}
+	console.log(`Sent ${newPosts.length} posts.`);
+	pushJobs.push(updateKV(list));
+	await Promise.all(pushJobs);
 }
 
 export async function onScheduled(_env: Env) {
-    env = _env;
-    const subs = SETTINGS.subscriptions;
-    for (const [uid, options] of Object.entries(subs)) {
-        try {
-            await processSingleUser(uid, options.webhookKeys, options.roles, options.language);
-        } catch (e) {
-            await processError((e as Error).message, options.webhookKeys)
-        }
-    }
+	env = _env;
+	const subs = SETTINGS.subscriptions;
+	const jobs = [];
+	for (const [uid, options] of Object.entries(subs)) {
+		jobs.push(processSingleUser(uid, options.webhookKeys, options.roles, options.language));
+	}
+	await Promise.all(jobs);
 }
