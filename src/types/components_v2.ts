1498fa1ad28734960e6d78bc684a26bcbad1e60c
GamerYuan <f2pplayer0801@gmail.com>
2025-04-28 23:05:59 +0800

    chore: update api types

--- src/types/components_v2.ts
+++ src/types/components_v2.ts
@@ -9,34 +9,49 @@ export interface Component {
 	id?: number;
 }
 
-export interface Button extends Component {
-	type: 2;
+export class Button implements Component {
+	type = 2;
 	/** 1 - Primary, 2 - Secondary, 3 - Success, 4 - Danger, 5 - Link, 6 - Premium */
-	style: 1 | 2 | 3 | 4 | 5 | 6;
+	style: number = 1;
 	label?: string;
 	sku_id?: string;
 	url?: string;
 	disabled?: boolean;
+
+	constructor(label?: string, style?: number, disabled?: boolean) {
+		this.label = label;
+		this.style = style ?? 1;
+		this.disabled = disabled ?? false;
+	}
 }
 
-export interface Section extends Component {
-	type: 9;
+export class Section implements Component {
+	type = 9;
 	/** Up to 3 Text Displays */
-	components: TextDisplay[];
+	components: TextDisplay[] = [];
 	accessory?: Thumbnail | Button;
 }
 
-export interface TextDisplay extends Component {
-	type: 10;
+export class TextDisplay implements Component {
+	type = 10;
 	content: string;
+
+	constructor(content: string) {
+		this.content = content;
+	}
 }
 
-export interface Thumbnail extends Component {
-	type: 11;
-	media: UnfurledMediaItem;
+export class Thumbnail implements Component {
+	type = 11;
+	media: UnfurledMediaItem = { url: '' };
 	description?: string;
 	/** Whether the container should be a spoiler (or blurred out). Defaults to false */
 	spoiler?: boolean;
+
+	constructor(media: UnfurledMediaItem, description?: string) {
+		this.media = media;
+		this.description = description;
+	}
 }
 
 export interface MediaGalleryItem {
@@ -46,23 +61,28 @@ export interface MediaGalleryItem {
 	spoiler?: boolean;
 }
 
-export interface MediaGallery extends Component {
-	type: 12;
-	items: MediaGalleryItem[];
+export class MediaGallery implements Component {
+	type = 12;
+	items: MediaGalleryItem[] = [];
 }
 
-export interface Separator extends Component {
-	type: 14;
+export class Separator implements Component {
+	type = 14;
 	/** Whether a visual divider should be displayed in the component. Defaults to true */
 	divider?: boolean;
 	/** 1 for small padding, 2 for large padding. Defaults to 1 */
 	spacing?: number;
+
+	constructor(spacing?: number, divider?: boolean) {
+		this.spacing = spacing ?? 1;
+		this.divider = divider ?? true;
+	}
 }
 
-export interface Container extends Component {
-	type: 17;
+export class Container implements Component {
+	type = 17;
 	/** Up to 10 components */
-	components: Component[];
+	components: Component[] = [];
 	/** Whether the container should be a spoiler (or blurred out). Defaults to false */
 	spoiler?: boolean;
 }
